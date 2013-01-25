6f2e6ddd319ed68d6775fc745103d423d162b85f
Shalini Sekhar <shalini@kairi.in>
2013-01-25 12:46:34 +0530

    .

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+    <html>
+            <head>
+                    <meta charset="utf-8" />
+                    <title>NCF Wireframe</title>
+                    <link href="styles.css" rel="stylesheet" type="text/css" />
+            </head>
+            <body>
+                   
+                    <header>
+                    	<img class="logo" src="/NCF-logo.gif">
+                    	<ul id="nav1">
+                    		<li>Explore
+                    			<ul>
+                    				<li><a href="map.html">By geography</a></li>
+                    				<li><a href="conflict.html">Conflict</a></li>
+                    				<li><a href="issue.html">Issue 2</a></li>
+                    				<li><a href="issue.html">Issue 3</a></li>
+                    				<li><a href="issue.html">Issue 4</a></li>
+                    				<li><a href="issue.html">Issue 5</a></li>
+                    			</ul>
+                    		</li>
+                    		<li>Programmes
+                    			<ul>
+                    				<li><a href="programme.html">Eastern Himalayas</a></li>
+                    				<li><a href="programme.html">High Altitude</a></li>
+                    				<li><a href="programme.html">Western Ghats</a></li>
+                    				<li><a href="programme.html">Education and Public Engagement</a></li>
+                    				<li><a href="programme.html">Primates</a></li>
+                    				<li><a href="programme.html">Oceans, Coasts and Rivers</a></li>
+                    				<li><a href="programme.html">Plains and Wetlands</a></li>
+                    				<li><a href="programme.html">Other Initiatives</a></li>
+                    			</ul>
+                    		</li>
+                    		<li><a href="people.html">People</a></li>
+                    		<li><a href="publications">Publications</a></li>
+                    	</ul>
+                    </header>
+                    <div id="homepage">
+                    	<p>This area will have featured content.</p>
+                    </div>
+                    
+
+
+            </body>
+    </html>
