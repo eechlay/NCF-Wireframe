3bed8ee452ac10e95b79365ea750724f5555ed42
Shalini Sekhar <shalini@kairi.in>
2013-01-27 12:07:03 +0530

    Added publications page

---
+++ publications.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+    <html>
+            <head>
+                    <meta charset="utf-8" />
+                    <title>NCF Wireframe</title>
+                    <link href="styles.css" rel="stylesheet" type="text/css" />
+                    <script type="text/javascript" src="//use.typekit.net/mhq1kzk.js"></script>
+                    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+            </head>
+            <body>
+                   <header>
+                   <?php include 'header.php'; ?>
+                    </header>
+
+                    <div id="people">
+                    	   <img src="/publications.gif">
+                    </div>
+                    <footer>
+                    <?php include 'footer.php'; ?>
+                    </footer
+            </body>
+    </html>
