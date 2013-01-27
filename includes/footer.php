981103be80274c6ce47e74ea3d70f75a82552fc7
Rahul Sekhar <sekhar.rahul@gmail.com>
2013-01-27 10:46:18 +0530

    changed html structure

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+        <footer>
+            <ul id="footernav">
+            	<li><a href="#">About NCF</a></li>
+            	<li><a href="#">News and events</a></li>
+            	<li><a href="#">Newsletter</a></li>
+            	<li><a href="#">Opportunities</a></li>
+            	<li><a href="#">Donate</a></li>
+            	<li><a href="#">Contact us</a></li>
+            </ul>
+        </footer>
+    </body>
+</html>
